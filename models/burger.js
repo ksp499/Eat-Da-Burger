0b962cc155ca211ccd3b334565daff14820c3019
ksp499 <ksp499@gmail.com>
2017-11-02 18:56:31 -0500

    finished except styling

--- models/burger.js
+++ models/burger.js
@@ -2,15 +2,21 @@ var orm = require("../config/orm.js");
 
 var burger = {
 	selectAll: function(cb) {
-		
+		orm.selectAll(function(res) {
+			cb(res);
+		});
 	},
 
-	insertOne: function(cb) {
-
+	insertOne: function(vals, cb) {
+		orm.insertOne(vals, function(res) {
+			cb(res);
+		});
 	},
 
-	updateOne: function(cb) {
-
+	updateOne: function(objColVals, condition, cb) {
+		orm.updateOne(objColVals, condition, function(res) {
+			cb(res);
+		});
 	}
 };
 
